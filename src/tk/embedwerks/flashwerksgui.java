e950ee1a1577b2777a4ed25457ad852a50118495
Shravan Jambukesan <shravan.j97@gmail.com>
2018-11-20 00:05:46 -0600

    * Support for Windows (requires Arduino for avrdude) * UI now renders in OS specific theme * Updated labels

--- src/tk/embedwerks/flashwerksgui.java
+++ src/tk/embedwerks/flashwerksgui.java
@@ -48,6 +48,8 @@ public class flashwerksgui extends javax.swing.JFrame
         selectDeviceLabel = new javax.swing.JLabel();
         deviceTypePicker = new javax.swing.JComboBox<>();
         jLabel1 = new javax.swing.JLabel();
+        embedwerksLabel = new javax.swing.JLabel();
+        mcuLabel = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -87,72 +89,72 @@ public class flashwerksgui extends javax.swing.JFrame
 
         jLabel1.setText("https://github.com/embedwerks/flashwerks");
 
+        embedwerksLabel.setText("(c) 2018 embedwerks");
+
+        mcuLabel.setText("Select microcontroller/microprocessor");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(devicePicker, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(14, 14, 14)
-                        .addComponent(selectDeviceLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 176, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(queryDevicesButton, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(fileChooserButton, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(deviceTypePicker, 0, 206, Short.MAX_VALUE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 90, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 345, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(32, 32, 32))
+                .addComponent(flashwerksLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 490, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(fileChooserButton, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(deviceTypePicker, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(queryDevicesButton, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(selectDeviceLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 176, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(devicePicker, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(mcuLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 90, Short.MAX_VALUE)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 345, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(32, 32, 32))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(programDeviceButton, javax.swing.GroupLayout.PREFERRED_SIZE, 251, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(versionLabel)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(embedwerksLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
                         .addComponent(jLabel2)
                         .addGap(54, 54, 54)
                         .addComponent(jLabel1))))
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(flashwerksLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 490, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(flashwerksLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(31, 31, 31))
-                    .addGroup(layout.createSequentialGroup()
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(20, 20, 20)
                         .addComponent(queryDevicesButton)
-                        .addGap(18, 18, 18)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(selectDeviceLabel)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(devicePicker, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(mcuLabel)
+                        .addGap(5, 5, 5)
                         .addComponent(deviceTypePicker, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(12, 12, 12)
+                        .addGap(18, 18, 18)
                         .addComponent(fileChooserButton)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)))
-                .addComponent(programDeviceButton, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(18, 18, 18)
+                        .addComponent(programDeviceButton, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(versionLabel)
                     .addComponent(jLabel2)
-                    .addComponent(jLabel1))
+                    .addComponent(jLabel1)
+                    .addComponent(embedwerksLabel))
                 .addContainerGap())
         );
 
@@ -163,16 +165,36 @@ public class flashwerksgui extends javax.swing.JFrame
         // TODO add your handling code here:
         try
         {
-            devicePicker.removeAllItems();
-            String cmd = "ls /dev/tty.*";
-            Process p = Runtime.getRuntime().exec(new String[]{"bash","-c",cmd});
-            p.waitFor();
-            BufferedReader buf = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            String line = "";
-            while ((line=buf.readLine())!=null)
+            if(System.getProperty("os.name").contains("Windows"))
             {
-                devicePicker.addItem(line);
+                devicePicker.removeAllItems();
+                Process p = Runtime.getRuntime().exec(new String[]{"cmd.exe", "/c", "mode"}); 
+                p.waitFor();
+                BufferedReader buf = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                String line = "";
+                while ((line=buf.readLine())!=null)
+                {
+                    if(line.contains("COM"))
+                    {
+                        String port = line.substring(line.indexOf("C"), line.indexOf(":"));
+                        devicePicker.addItem(port);
+                    }
+                }
             }
+            else
+            {
+                devicePicker.removeAllItems();
+                String cmd = "ls /dev/tty.*";
+                Process p = Runtime.getRuntime().exec(new String[]{"bash","-c", cmd});
+                p.waitFor();
+                BufferedReader buf = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                String line = "";
+                while ((line=buf.readLine())!=null)
+                {
+                    devicePicker.addItem(line);
+                }
+            }
+
         }
         catch(Exception e)
         {
@@ -191,7 +213,6 @@ public class flashwerksgui extends javax.swing.JFrame
             {
                 File selectedFile = fc.getSelectedFile();
                 filePath = selectedFile.getCanonicalPath();
-                System.out.println(filePath);
             }
         }
         catch(Exception e)
@@ -205,6 +226,24 @@ public class flashwerksgui extends javax.swing.JFrame
 
         try
         {
+            String avrDudePath = "";
+            String avrDudeCommand = "";
+            
+            /*
+            Requires you to copy the avrdude.conf from C:\Program Files (x86)\Arduino\hardware\tools\avr\etc to 
+            C:\Program Files (x86)\Arduino\hardware\tools\avr\bin
+            */
+            if(System.getProperty("os.name").contains("Windows"))
+            {
+                avrDudePath = "C:\\Program Files (x86)\\Arduino\\hardware\\tools\\avr\\bin";
+                avrDudeCommand = "\"C:\\Program Files (x86)\\Arduino\\hardware\\tools\\avr\\bin\\avrdude\"";
+            }
+            else
+            {
+                avrDudePath = "/usr/local/bin/";
+                avrDudeCommand = "/usr/local/bin/avrdude";
+            }
+            
             int devicePickerIndex = devicePicker.getSelectedIndex();
             port = devicePicker.getItemAt(devicePickerIndex);
             System.out.println(port);
@@ -213,7 +252,7 @@ public class flashwerksgui extends javax.swing.JFrame
             String cmd = " -v -p " + deviceType + " -c arduino -P " + port + " -b 115200 -D -Uflash:w:" + filePath + ":i";
             System.out.println(cmd);
             List<String> commands = new ArrayList<String>();
-            commands.add("/usr/local/bin/avrdude");
+            commands.add(avrDudeCommand);
             commands.add("-v");
             commands.add("-p");
             commands.add(deviceType);
@@ -226,7 +265,7 @@ public class flashwerksgui extends javax.swing.JFrame
             commands.add("-D");
             commands.add("-Uflash:w:" + filePath + ":i");
             ProcessBuilder pb = new ProcessBuilder(commands);
-            pb.directory(new File("/usr/local/bin/"));
+            pb.directory(new File(avrDudePath));
             pb.redirectErrorStream(true);
             Process process = pb.start();
             StringBuilder out = new StringBuilder();
@@ -260,25 +299,24 @@ public class flashwerksgui extends javax.swing.JFrame
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
+        try 
+        {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } 
+        catch (ClassNotFoundException ex)
+        {
             java.util.logging.Logger.getLogger(flashwerksgui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
+        } 
+        catch (InstantiationException ex)
+        {
             java.util.logging.Logger.getLogger(flashwerksgui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
+        }
+        catch (IllegalAccessException ex)
+        {
             java.util.logging.Logger.getLogger(flashwerksgui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } 
+        catch (javax.swing.UnsupportedLookAndFeelException ex)
+        {
             java.util.logging.Logger.getLogger(flashwerksgui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
@@ -294,11 +332,13 @@ public class flashwerksgui extends javax.swing.JFrame
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox<String> devicePicker;
     private javax.swing.JComboBox<String> deviceTypePicker;
+    private javax.swing.JLabel embedwerksLabel;
     private javax.swing.JButton fileChooserButton;
     private javax.swing.JLabel flashwerksLabel;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel mcuLabel;
     private javax.swing.JButton programDeviceButton;
     private javax.swing.JButton queryDevicesButton;
     private javax.swing.JLabel selectDeviceLabel;
